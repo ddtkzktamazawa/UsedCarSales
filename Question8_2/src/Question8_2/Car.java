45ee34494135c4f57c671f5ee117dc85363d51d2
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-04-28 18:56:11 +0900

    演習8-2終わりました

--- Question8_2/src/Question8_2/Car.java
+++ Question8_2/src/Question8_2/Car.java
@@ -6,13 +6,21 @@ package Question8_2;
  * 作成日            : 2026.4.28
  */
 public class Car {
+	//名前の情報を格納
 	private String nameInformation;
+	//横幅の情報を格納
 	private int widthInformation;
+	//高さの情報を格納
 	private int heigthInformation;
+	//長さの情報を格納
 	private int lengthInformation;
+	//x座標の情報を格納
 	private double xCoordinate;
+	//y座標の情報を格納
 	private double yCoordinate;
+	//燃料の情報を格納
 	private double fuelInformation;
+	//ナンバーの情報を格納
 	private int numberInformation;
 
 	/**
@@ -25,16 +33,26 @@ public class Car {
 	 * 作成日           : 2026.4.28
 	 */
 	public Car(String nameInformation, int widthInformation, int heigthInformation, int lengthInformation, double xCoordinate, double yCoordinate, double fuelInformation, int numberInformation) {
+		//フィールドと引数が同名のためthisを使ってインスタンス変数に代入
 		this.nameInformation = nameInformation;
+		//フィールドと引数が同名のためthisを使ってインスタンス変数に代入
 		this.widthInformation = widthInformation;
+		//フィールドと引数が同名のためthisを使ってインスタンス変数に代入
 		this.heigthInformation = heigthInformation;
+		//フィールドと引数が同名のためthisを使ってインスタンス変数に代入
 		this.lengthInformation = lengthInformation;
+		//フィールドと引数が同名のためthisを使ってインスタンス変数に代入
 		this.fuelInformation = fuelInformation;
+		//フィールドと引数が同名のためthisを使ってインスタンス変数に代入
 		this.numberInformation = numberInformation;
+		//x,y座標の指定
 		xCoordinate = yCoordinate = 0.0;
 	}
+	//x座標を取得する
 	double getX() {return xCoordinate;}
+	//y座標を取得する
 	double getY() {return yCoordinate;}
+	//燃料量を取得する
 	double getFuel() {return fuelInformation;}
 
 	/**
@@ -46,6 +64,7 @@ public class Car {
 	 * 作成日           : 2026.4.28
 	 */
 	public String getName() {
+		//名前情報を返却
 		return nameInformation;
 	}
 
@@ -58,6 +77,7 @@ public class Car {
 	 * 作成日           : 2026.4.28
 	 */
 	public int getwidth() {
+		//幅の情報を返却
 		return widthInformation;
 	}
 
@@ -70,6 +90,7 @@ public class Car {
 	 * 作成日           : 2026.4.28
 	 */
 	public int getheigth() {
+		//高さの情報を返却
 		return heigthInformation;
 	}
 
@@ -82,6 +103,7 @@ public class Car {
 	 * 作成日           : 2026.4.28
 	 */
 	public int getlength() {
+		//長さの情報を返却
 		return lengthInformation;
 	}
 
@@ -94,19 +116,10 @@ public class Car {
 	 * 作成日           : 2026.4.28
 	 */
 	public int getNumber() {
+		//ナンバーの情報を返却
 		return numberInformation;
 	}
-	/**
-	 * 関数名           : reFuel
-	 * メソッドの説明   : 指定された量だけ燃料を補給する
-	 * パラメータの説明 : 補給する量
-	 * 返り値           : なし
-	 * 作成者           : 玉澤一輝
-	 */
-	public void reFuel(double fuelInformation) {
-		this.fuelInformation += fuelInformation;
-		System.out.println(fuelInformation + "L 給油しました。");
-	}
+
 	/**
 	 * 関数名           : printSpec
 	 * メソッドの説明   : インスタンスが持つプロパティ（名前・幅・高さ・長さ・ナンバー）を表示する
@@ -116,20 +129,33 @@ public class Car {
 	 * 作成日           : 2026.4.28
 	 */
 	public void printSpec() {
+		//"名前：nameInformation"を表示して改行する
 		System.out.println("名前：" + nameInformation);
+		//"車幅：widthInformation"を表示して改行する
 		System.out.println("車幅：" + widthInformation + "mm");
+		//"車高：heigthInformation"を表示して改行する
 		System.out.println("車高：" + heigthInformation + "mm");
+		//"車長：lengthInformation"を表示して改行する
 		System.out.println("車長：" + lengthInformation + "mm");
+		//"ナンバー：numberInformation"を表示して改行する
 		System.out.println("ナンバー：" + numberInformation);
 	}
 	boolean move(double dx, double dy) {
-		double dist = Math.sqrt(dx * dx + dy * dy);
-		if(dist > fuelInformation) {
+		//移動距離（直線距離）を計算する
+		double distValue = Math.sqrt(dx * dx + dy * dy);
+		//もしdistValueがfuelInformationよりも大きい場合
+		if(distValue > fuelInformation) {
+			//falseを返却
 			return false;
+			//その他の場合
 		}else {
-			fuelInformation -= dist;
+			//fuelInformationからdistValueを引く
+			fuelInformation -= distValue;
+			//xCoordinateにdxを加える
 			xCoordinate += dx;
+			//yCoordinateにdyを加える
 			yCoordinate += dy;
+			//trueを返却
 			return true;
 		}
 	}
--- Question8_2/src/Question8_2/Question8_2.java
+++ Question8_2/src/Question8_2/Question8_2.java
@@ -1,34 +1,48 @@
 package Question8_2;
-
 import java.util.Scanner;
 
 public class Question8_2 {
+	//キーボードで入力した値を入れる変数を作る（プログラム全体で使用し外部から書き換えられない）
 	private static final Scanner STANDARD_INPUT = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// インスタンス生成（最大燃料 64.0L）
-		Car porsche = new Car("ポルシェ", 1900, 1300, 4500, 0, 0, 64, 1234);
-		Car benz = new Car("ベンツ", 1800, 1550, 4400, 50, 50, 50, 9876);
-
-		porsche.printSpec();
+		//変数porscheCarに入力したデータを読み込む
+		Car porscheCar = new Car("ポルシェ", 1900, 1300, 4500, 0, 0, 64, 1234);
+		//変数benzCarに入力したデータを読み込む
+		Car benzCar = new Car("ベンツ", 1800, 1550, 4400, 50, 50, 50, 9876);
+		//メソッドprintSpecを呼び出す
+		porscheCar.printSpec();
+		//改行する
 		System.out.println();
-		benz.printSpec();
-
+		//メソッドprintSpecを呼び出す
+		benzCar.printSpec();
+		//繰り返す
 		while (true) {
+			//改行する
 			System.out.println();
-			System.out.println("【" + porsche.getName() + "の移動】");
-			System.out.println("現在地("+ porsche.getX() + ", " + porsche.getY() + ")・残り燃料" + porsche.getFuel());
-
+			//"ポルシェの移動"と表示して改行する
+			System.out.println("【" + porscheCar.getName() + "の移動】");
+			//"現在地(0.0, 0.0)・残り燃料64.0"と表示して改行する
+			System.out.println("現在地("+ porscheCar.getX() + ", " + porscheCar.getY() + ")・残り燃料" + porscheCar.getFuel());
+			//"移動しますか[0…No/1…Yes]："と表示して改行する
 			System.out.print("移動しますか[0…No/1…Yes]：");
-			if (STANDARD_INPUT.nextInt() == 0) break;
-
+			//もし入力した数値が0の場合
+			if (STANDARD_INPUT.nextInt() == 0) 
+				//抜ける
+				break;
+			//"x方向の移動距離："と表示する
 			System.out.print("x方向の移動距離：");
+			//変数dxに数値を代入する
 			double dx = STANDARD_INPUT.nextDouble();
+			//"y方向の移動距離："と表示する
 			System.out.print("y方向の移動距離：");
+			//変数dyに数値を代入する
 			double dy = STANDARD_INPUT.nextDouble();
-
-			if (!porsche.move(dx, dy)) {
+			//指定された距離(dx, dy)の移動ができない場合
+			if (!porscheCar.move(dx, dy)) {
+				//"燃料が足りません。ドライブを終了します。"と表示するして改行する
 				System.out.println("燃料が足りません。ドライブを終了します。");
+				//抜ける
 				break;
 			}
 		}
