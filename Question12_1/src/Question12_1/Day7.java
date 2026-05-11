2f8c46b8610e7d0fb839205a143c95656bcd068c
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-05-11 18:15:06 +0900

    演習12-1終わりました

---
+++ Question12_1/src/Question12_1/Day7.java
@@ -0,0 +1,187 @@
+package Question12_1;
+
+/*
+ * クラス名          : Day7
+ * 概要              : 日付情報を管理し曜日計算や文字列表現の提供を行うクラス
+ * 					　 5種類のコンストラクタによる初期化とカプセル化（ゲッタ・セッタ）を
+ * 					　 実装している
+ * 作成者            : 玉澤一輝
+ * 作成日            : 2026.05.11
+ */
+public class Day7 {
+	//年の初期化
+	private int yearNumber = 1;
+	//月の初期化
+	private int monthNumber = 1;
+	//日付の初期化
+	private int dateNumber = 1;
+
+	/**
+	 * コンストラクタ   	  : Day7
+	 * コンストラクタの説明   : 指定された年月日、または他のDayインスタンスを基に初期化を行う
+	 * 							年が0の場合は1年として処理する
+	 * パラメータの説明       : 年、年月、年月日、Dayインスタンス
+	 * 返り値                 : なし
+	 * 作成者                 : 玉澤一輝
+	 * 作成日                 : 2026.5.11
+	 */
+	//フィールドの初期値でインスタンスを生成する
+	public Day7() {}
+	//年を指定してを受け取って初期化
+	public Day7(int year) {
+		//もし指定された年が0の場合、1年とする
+		if(year == 0)year =1; this.yearNumber = year;
+	}
+	//年と月を指定する
+	public Day7(int year, int month) {
+		//年指定のコンストラクタを呼び出して処理
+		this(year); this.monthNumber = month;
+	}
+	//年と月と日を指定する
+	public Day7(int year, int month, int date) {
+		//年月指定のコンストラクタを呼び出して処理
+		this(year, month); this.dateNumber = date;
+	}
+	//別のインスタンスを受け取って自身の値としてコピー
+	public Day7(Day7 d) {
+		//年月日指定のコンストラクタを呼び出して複製
+		this(d.yearNumber, d.monthNumber, d.dateNumber);
+	}
+	/**
+	 * 関数名           : getYear
+	 * メソッドの説明   : 年のフィールド値を返却する
+	 * パラメータの説明 : なし
+	 * 返り値           : 年
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public int getYear() {
+		//年を返却
+		return yearNumber;
+	}
+	/**
+	 * 関数名           : getMonth
+	 * メソッドの説明   : 月のフィールド値を返却する
+	 * パラメータの説明 : なし
+	 * 返り値           : 月
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public int getMonth() {
+		//月を返却
+		return monthNumber;
+	}
+	/**
+	 * 関数名           : getDate
+	 * メソッドの説明   : 日にちのフィールド値を返却する
+	 * パラメータの説明 : なし
+	 * 返り値           : 日にち
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public int getDate() {
+		//日にちを返却
+		return dateNumber;
+	}	
+	/**
+	 * 関数名           : setYear
+	 * メソッドの説明   : 年の値を更新する
+	 * パラメータの説明 : 年
+	 * 返り値           : なし
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public void setYear(int year) {
+		//yearNumberに引数の値を代入する
+		this.yearNumber = year;
+	}
+	/**
+	 * 関数名           : setMonth
+	 * メソッドの説明   : 月の値を更新する
+	 * パラメータの説明 : 月
+	 * 返り値           : なし
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public void setMonth(int month) {
+		//monthNumberに引数の値を代入する
+		this.monthNumber = month;
+	}
+	/**
+	 * 関数名           : setDate
+	 * メソッドの説明   : 日付の値を更新する
+	 * パラメータの説明 : 日付
+	 * 返り値           : なし
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public void setdate(int date) {
+		//setDateに引数の値を代入する
+		this.dateNumber = date;
+	}
+	/**
+	 * 関数名           : set
+	 * メソッドの説明   : 年月日をまとめて更新する
+	 * パラメータの説明 : 年、月、日
+	 * 返り値           : なし
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public void set(int year, int month, int date) {
+		//yearNumberに引数の値を代入する
+		this.yearNumber = year;
+		//monthNumberに引数の値を代入する
+		this.monthNumber = month;
+		//dateNumberに引数の値を代入する
+		this.dateNumber = date;
+	}
+	/**
+	 * 関数名           : dayOfWeek
+	 * メソッドの説明   : 曜日を算出する
+	 * パラメータの説明 : なし
+	 * 返り値           : 0(日曜日)～6(土曜日)の整数値
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public int dayOfWeek() {
+		//フィールドの値をコピー
+		int yearValue = yearNumber;
+		//フィールドの値をコピー
+		int monthValue = monthNumber;
+		//もし月が1月または2月の場合
+		if(monthValue == 1 || monthValue == 2) {
+			//yearValueをデクリメント
+			yearValue--;
+			//monthValueに12を加える
+			monthValue += 12;
+		}
+		//曜日計算結果を返却
+		return(yearValue + yearValue / 4 - yearValue / 100 + yearValue / 400 + (13 * monthValue + 8) / 5 + dateNumber) % 7;
+	}
+	/**
+	 * 関数名           : equalTo
+	 * メソッドの説明   : 他のDayインスタンスと日付が一致するか判定する
+	 * パラメータ       : 比較対象のDayインスタンスd
+	 * 返り値           : 一致すればtrue、しなければfalse
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public boolean equalTo(Day7 d) {
+		//年、月、日がすべて等しいか判定した結果を返す
+		return yearNumber == d.yearNumber && monthNumber == d.monthNumber && dateNumber == d.dateNumber;
+	}
+	/**
+	 * 関数名           : toString
+	 * メソッドの説明   : 現在の日付情報の文字列表現を返却する
+	 * パラメータ       : なし
+	 * 返り値           :「yyyy年mm月dd日（曜）」形式の文字列
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.5.11
+	 */
+	public String toString() {
+		//曜日を表示するための文字配列
+		String[] weekDay = {"日","月","火","水","木","金","土"};
+		//4桁・2桁・2桁で整形しdayOfWeekの結果を曜日に入れる
+		return String.format("%04d年%02d月%02d日（%s）", yearNumber, monthNumber, dateNumber, weekDay[dayOfWeek()]);
+	}
+}
---
+++ Question12_1/src/Question12_1/ExCar.java
@@ -0,0 +1,64 @@
+package Question12_1;
+
+/*
+ * クラス名          : ExCar
+ * 概要              : 自動車クラス Car（第2版）から派生したクラス
+ * 					   総走行距離の管理機能を追加している
+ * 作成者            : 玉澤一輝
+ * 作成日            : 2026.05.11
+ */
+public class ExCar extends Car2 {
+	//総走行距離の情報を格納する
+	private double totalMileage;
+
+	/**
+	 * コンストラクタ名   	  : ExCar
+	 * コンストラクタの説明   : 車の名前、サイズ、燃料、購入日を初期化し、
+	 * 							「総走行距離を用いて親クラスのコンストラクタを呼び出し
+	 * 							基本情報を設定した後、自クラスのフィールドを初期化する
+	 * パラメータ       	  : 名前、幅、高さ、長さ、燃料、購入日
+	 * 返り値           	  : なし
+	 * 作成者           	  : 玉澤一輝
+	 * 作成日           	  : 2026.05.11
+	 */
+	public ExCar(String nameInformation, int widthInformation, int heightInformation, int lengthInformation, double fuelInformation, Day7 purchaseDay) {
+		//親クラスのコンストラクタを呼び出す
+		super(nameInformation, widthInformation, heightInformation, lengthInformation, fuelInformation, purchaseDay);
+		//総走行距離を0.0で新しく作成する
+		this.totalMileage = 0.0;
+	}
+
+	/**
+	 * メソッド名       : getTotalMileage
+	 * メソッドの説明   : 総走行距離を返却する
+	 * パラメータ       : なし
+	 * 返り値           : 総走行距離
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.05.11
+	 */
+	public double getTotalMileage() {
+		//totalMileageを返却する
+		return totalMileage;
+	}
+	/**
+	 * メソッド名       : move
+	 * メソッドの説明   : 指定された距離を移動し、成功した場合は総走行距離を加算する
+	 * パラメータ       : dx、dy
+	 * 返り値           : 移動成功ならtrue、燃料不足ならfalse
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.05.11
+	 */
+	public boolean move(double dx, double dy) {
+		//moveメソッドを呼び出す
+		boolean success = super.move(dx, dy);
+		//もし移動に成功した場合
+		if (success) {
+			//移動距離（直線距離）を計算する
+			double distanceTraveled = Math.sqrt(dx * dx + dy * dy);
+			//総走行距離を更新する
+			totalMileage += distanceTraveled;
+		}
+		//移動の結果を返却する
+		return success; 
+	}
+}
