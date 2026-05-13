390e567425762ba34b9956cce6d7f13b4e00d0cd
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-05-13 17:51:05 +0900

    演習14-1終わりました

---
+++ Question14_1/src/Question14_1/Plane2D.java
@@ -0,0 +1,18 @@
+package Question14_1;
+/**
+ * インタフェース名		:Plane2D
+ * 概要    				:図形の面積を表すインタフェース
+ * @author  			:玉澤一輝
+ * 作成日  				:2026.5.13
+ */
+public interface Plane2D {
+	/**
+	 * 関数名           	:getArea
+	 * メソッドの説明   	:面積の値を求める
+	 * @param 				:なし
+	 * @return           	:なし
+	 * @author  			:玉澤一輝
+	 * 作成日  				:2026.5.13
+	 */
+	int getArea();
+}
