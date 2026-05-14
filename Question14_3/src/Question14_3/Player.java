896ad9205b1f05fd34ac795bcb4f4a70a8893dd1
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-05-14 14:56:52 +0900

    演習14-3終わりました

---
+++ Question14_3/src/Question14_3/Player.java
@@ -0,0 +1,20 @@
+package Question14_3;
+
+public interface Player {
+	/**
+	 * 関数名           : play
+	 * メソッドの説明	: 再生する
+	 * @param 			: なし
+	 * @return 			: なし
+	 * 作成日			: 2026.05.14
+	 */
+	void play();
+	/**
+	 * 関数名           : stop
+	 * メソッドの説明	: 停止する
+	 * @param 			: なし
+	 * @return 			: なし
+	 * 作成日			: 2026.05.14
+	 */
+	void stop();
+}
