1e417955bf5d3e29959da44eb3a8d62fd9c86ec6
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-05-15 14:43:56 +0900

    演習5-16終わりました

---
+++ Question15_6/src/Question15_6/circleCircumferenceAndArea.java
@@ -0,0 +1,31 @@
+package Question15_6;
+/**
+ * クラス名     : circleCircumferenceAndArea
+ * 概要			: コマンドライン引数で与えられた半径を持つ円周の長さと面積を求めて表示する
+ * @author 		: 玉澤 一輝
+ * 作成日		: 2026.05.15
+ */
+public class circleCircumferenceAndArea {
+	/**
+	 * 関数名			: main
+	 * メソッドの説明	: コマンドライン引数で与えられた半径を持つ円周の長さと面積を計算して表示する
+	 * @param       	: args(コマンドライン引数、円の半径を文字列として受け取る）
+	 * @return         	: なし
+	 * @author         : 玉澤一輝
+	 * 作成日           : 2026.05.15
+	 */
+	public static void main(String[] args) {
+		//double型変数PIValue（円周率）をfinal変数で定義する
+		final double PIValue = 3.1416;
+		//double型変数inputValueの初期化
+		double inputValue = 0.0;
+		//変数iが0から始めて1ずつ増やしていきargs.lengthまで繰り返す
+		for(int i = 0; i < args.length; i++)
+			//変数inputValueにコマンドライン引数として受け取った文字列をdouble型に変換して代入する
+			inputValue = Double.parseDouble(args[i]);
+		//"円の円周の長さは inputValue * 2 * PIValueです。"と表示して改行する
+		System.out.println("円の円周の長さは " + inputValue * 2 * PIValue + " です。");
+		//"円の面積は inputValue * inputValue * PIValue です。"と表示して改行する
+		System.out.println("円の面積は " + inputValue * inputValue * PIValue + " です。");
+	}
+}
