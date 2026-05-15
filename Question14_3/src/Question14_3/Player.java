e3f7c7d2c472989286c9abddb1a8628295df680a
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-05-15 14:15:40 +0900

    演習14-3修正しました

--- Question14_3/src/Question14_3/Player.java
+++ Question14_3/src/Question14_3/Player.java
@@ -1,5 +1,10 @@
 package Question14_3;
-
+/**
+ * クラス名          : Player
+ * 概要              : 再生および停止の機能の定義
+ * @author     	     : 玉澤 一輝
+ * 作成日            : 2026.05.14
+ */
 public interface Player {
 	/**
 	 * 関数名           : play
