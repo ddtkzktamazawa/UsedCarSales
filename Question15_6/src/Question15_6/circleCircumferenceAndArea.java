6cdc335b43f3b583261a2780fc5583ddb57793c6
ddtkzktamazawa <ddt.kzk.tamazawa@gmail.com>
2026-05-18 14:18:56 +0900

    演習15-6修正しました

--- Question15_6/src/Question15_6/circleCircumferenceAndArea.java
+++ Question15_6/src/Question15_6/circleCircumferenceAndArea.java
@@ -15,6 +15,13 @@ public class circleCircumferenceAndArea {
 	 * 作成日           : 2026.05.15
 	 */
 	public static void main(String[] args) {
+		//もしargs.length（要素数）が0と一致する場合
+		if (args.length == 0) {
+			//"エラー：円の半径をコマンドライン引数に指定してください。"と表示して改行する
+			System.out.println("エラー：円の半径をコマンドライン引数に指定してください。");
+			//返却する
+			return;
+		}
 		//double型変数PIValue（円周率）をfinal変数で定義する
 		final double PIValue = 3.1416;
 		//double型変数inputValueの初期化
